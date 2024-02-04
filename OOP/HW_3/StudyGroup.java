77d12c0db993754950744dab27093a968fafd832
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2024-02-04 12:54:51 +0300

    StudyGroup.java

---
+++ OOP/HW_3/StudyGroup.java
@@ -0,0 +1,14 @@
+package Sem_3;
+
+class StudyGroup {
+
+    private int num;
+
+    public StudyGroup (int num){
+        this.num = num;
+    }
+
+    public int getNum(){
+        return num;
+    }
+}
