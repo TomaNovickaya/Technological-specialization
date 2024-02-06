c7fc199e4a79a30691d15ba984d60403268c17ce
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2024-02-06 17:53:36 +0300

    User.java

---
+++ OOP/HW_4/User.java
@@ -0,0 +1,36 @@
+public class User {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    
+    public User(String firstName, String lastName, String middleName){
+        this.firstNmae = firstNmae;
+        this.lastName = lastName;
+        this.middleName = middleName;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    pablic void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+    
+    pablic void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    
+    public String getMiddleName(){
+        return middleName;
+    }
+    
+    pablic void setMiddleName(String middleName){
+        this.middleName = middleName;
+    }
+    
+}
