9695aacd27a74f2b86e83b17a7f2b4648428cea0
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2024-02-16 11:04:19 +0300

    Main.java

---
+++ Unit_tests/HW_2/Test/seminars/second/hw/Main.java
@@ -0,0 +1,12 @@
+package seminars.second.hw;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
