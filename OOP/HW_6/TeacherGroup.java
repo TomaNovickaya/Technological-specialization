904c41532b3e678533b4fafcc72c90bcc10a853e
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2024-02-20 11:17:47 +0300

    TeacherGroup.java

---
+++ OOP/HW_6/TeacherGroup.java
@@ -0,0 +1,21 @@
+package HW_6;
+
+import java.util.Iterator;
+import java.util.List;
+
+// Принцип инверсии зависимостей
+public class TeacherGroup implements Iterable<Teacher> {
+    private List<Teacher> teacherList;
+    public List<Teacher> getTeacherList() {
+        return teacherList;
+    }
+    public void setTeacherList(List<Teacher> teacherList) {
+        this.teacherList = teacherList;
+    }
+    public void createTeacher(String firstName, String lastName, String middleName){
+        teacherList.add(new Teacher(firstName, lastName, middleName));
+    }
+    public Iterator<Teacher> iterator() {
+        return new TeacherGroupIterator(this);
+    }
+}
