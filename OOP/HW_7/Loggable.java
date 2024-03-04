e4a12f81f7070e79d2083f960e2c0e07d8463efe
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2024-03-04 11:55:07 +0300

    Loggable.java

--- OOP/HW_7/Loggable.java
+++ OOP/HW_7/Loggable.java
@@ -1,10 +1,5 @@
-public class LogCalculableFactory implements ICalculableFactory{
-    private Loggable logger;
-    public LogCalculableFactory(Loggable logger){
-        this.logger = logger;
-    }
-    @Override
-    public Calculable create(int primaryArg){
-        return new LogCalculator(new Calculator(primaryArg), logger);
-    }
+public interface Loggable {
+
+    void log(String message);
 }
+
