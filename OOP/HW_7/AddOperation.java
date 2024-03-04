4911016ae5f27a790ed2ee86ac49e172cefd2c59
TomaNovickaya <126395023+TomaNovickaya@users.noreply.github.com>
2024-03-04 11:49:19 +0300

    AddOperation.java

---
+++ OOP/HW_7/AddOperation.java
@@ -0,0 +1,5 @@
+public class AddOperation implements ComplexOperation {
+    public ComplexNumber operate(ComplexNumber a, ComplexNumber b) {
+        return new ComplexNumber(a.getReal() + b.getReal(), a.getImaginary() + b.getImaginary());
+    }
+}
